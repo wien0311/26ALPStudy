ae1db0b9d797114ad82e7c7621de03402ca2edfd
wien0311 <nmnm12070311@gmail.com>
2026-09-25 21:31:59 +0900

    2week folder + circleK)

---
+++ 2026-2/Basic/wien0311/2 week/editor(difficult).c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char editor[100001],arr[5];
+    int m=0, cl;
+
+    scanf("%s",editor);
+    scanf("%d",&m);
+    cl=strlen(editor)+1;
+
+    for(int i=0; i<m; i++) {
+        scanf("%s",arr);
+        if(arr[0]=='L') {
+
+        }
+        else if(arr[0]=='D') {
+
+        }
+        else if(arr[0]=='B') {
+
+        }
+        else if(arr[0]=='P') {
+
+        }
+    }
+}
